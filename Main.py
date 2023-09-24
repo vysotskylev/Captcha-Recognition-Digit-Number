74ca595ad35a9859f7a5a1cee3610727f17fedb3
Your Name <you@example.com>
2023-09-25 00:41:35 +0200

    WIP

--- Main.py
+++ Main.py
@@ -8,6 +8,7 @@ import time
 import sys
 import os
 import random
+import re
 
 import tensorflow as tf
 
@@ -20,22 +21,24 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 digits_symbols = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
+CHECKPOINT_PREFIX = "crack_captcha_"
+
 CHAR_NUM = 10
+LEARNING_RATE_DROP_ITERS = 100
+CHECKPOINT_ITERS = 100
+STAT_PRINT_ITERS = 10
 
 
 # 1---------------------
-def generate_folder():
-    if not os.path.exists("./image"):
-        os.makedirs("./image")
-    for i in os.listdir("./image"):
-        if i == "tfrecord":
-            continue
-        os.remove(os.path.join("./image", i))
-
-    if not os.path.exists("./image/tfrecord"):
-        os.makedirs("./image/tfrecord")
-    for i in os.listdir("./image/tfrecord"):
-        os.remove(os.path.join("./image/tfrecord", i))
+def generate_folder(data_dir):
+    image_dir = os.path.join(data_dir, "image")
+    os.makedirs(image_dir, exist_ok=True)
+    for i in os.listdir(image_dir):
+        os.remove(os.path.join(image_dir, i))
+    tfrecord_dir = os.path.join(data_dir, "tfrecord")
+    os.makedirs(tfrecord_dir, exist_ok=True)
+    for i in os.listdir(tfrecord_dir):
+        os.remove(os.path.join(tfrecord_dir, i))
 
 
 def random_captcha_text(char_set, captcha_size):
@@ -46,33 +49,34 @@ def random_captcha_text(char_set, captcha_size):
     return captcha_text
 
 
-def gen_captcha_text_and_image(idx, num_digits):
+def gen_captcha_text_and_image(idx, num_digits, data_dir):
     image = ImageCaptcha(width=200, height=50, font_sizes=(40,))
     captcha_text = random_captcha_text(digits_symbols, num_digits)
     captcha_text = "".join(captcha_text)
-    image.write(captcha_text, f"./image/{idx:06}_{captcha_text}.png")  # write it
+    image.write(captcha_text, os.path.join(data_dir, "image", f"{idx:06}_{captcha_text}.png"))  # write it
 
 
 # 2------------------
-def generate_train_data(num_samples, num_digits):
+def generate_train_data(num_samples, num_digits, data_dir):
     for i in range(num_samples):
-        gen_captcha_text_and_image(i, num_digits)
+        gen_captcha_text_and_image(i, num_digits, data_dir)
         sys.stdout.write("\r>>creating images %d/%d" % (i + 1, num_samples))
         sys.stdout.flush()
     sys.stdout.write("\n")
     sys.stdout.flush()
-    print("All picture has been generated")
+    print("All pictures has been generated")
 
 
 # 3--------------------
-def save_as_tf(num_test):
+def save_as_tf(num_test, data_dir):
     _RANDOM_SEED = 0
-    DATASET_DIR = "./image/"
-    TFRECORD_DIR = "./image/tfrecord/"
+    DATASET_DIR = os.path.join(data_dir, "image")
+    VALIDATION_DIR = os.path.join(data_dir, "validation")
+    TFRECORD_DIR = os.path.join(data_dir, "tfrecord")
 
     def _dataset_exists(dataset_dir):
-        for split_name in ["train", "test"]:
-            output_filename = os.path.join(dataset_dir, split_name + "tfrecords")
+        for split_name in ["train", "test", "validation"]:
+            output_filename = os.path.join(dataset_dir, split_name + ".tfrecords")
             if not tf.io.gfile.exists(output_filename):
                 return False
         return True
@@ -102,8 +106,8 @@ def save_as_tf(num_test):
             )
         )
 
-    def _convert_dataset(split_name, filenames, dataset_dir):
-        assert split_name in ["train", "test"]
+    def _convert_dataset(split_name, filenames):
+        assert split_name in ["train", "test", "validation"]
 
         with tf.compat.v1.Session() as sess:
             output_filename = os.path.join(TFRECORD_DIR, split_name + ".tfrecords")
@@ -133,56 +137,88 @@ def save_as_tf(num_test):
             sys.stdout.write("\n")
             sys.stdout.flush()
 
-    if _dataset_exists(DATASET_DIR):
+    if _dataset_exists(TFRECORD_DIR):
         print("file already exists")
     else:
         photo_filenames = _get_filenames_and_classes(DATASET_DIR)
+        validation_filenames = _get_filenames_and_classes(VALIDATION_DIR)
 
         random.seed(_RANDOM_SEED)
         random.shuffle(photo_filenames)
         training_filenames = photo_filenames[num_test:]
         testing_filenames = photo_filenames[:num_test]
 
-        _convert_dataset("train", training_filenames, DATASET_DIR)
-        _convert_dataset("test", testing_filenames, DATASET_DIR)
+
+        _convert_dataset("train", training_filenames)
+        _convert_dataset("test", testing_filenames)
+        _convert_dataset("validation", validation_filenames)
         print("-------------We have produced all tfrecord file------------------")
 
 
+def read_and_decode(filename, num_digits):
+    filename_queue = tf.compat.v1.train.string_input_producer([filename])
+    reader = tf.compat.v1.TFRecordReader()
+    _, serialized_example = reader.read(filename_queue)
+    features = tf.io.parse_single_example(
+        serialized_example,
+        features={
+            "image": tf.io.FixedLenFeature([], tf.string),
+            "labels": tf.io.FixedLenFeature([num_digits], tf.int64),
+        },
+    )
+    image = tf.io.decode_raw(features["image"], tf.uint8)
+    image_raw = tf.reshape(image, [224, 224])  # raw data
+
+    image = tf.reshape(image, [224, 224])
+    image = tf.cast(image, tf.float32) / 255.0  # standardlize
+    image = tf.subtract(image, 0.5)
+    image = tf.multiply(image, 2.0)
+
+    labels = tf.cast(features["labels"], tf.int32)
+    return image, image_raw, labels
+
+
 # 4-------------
-def train(num_digits, batch_size):
+def train(num_digits, batch_size, data_dir):
     tf.compat.v1.reset_default_graph()
-    TFRECORD_FILE = "./image/tfrecord/train.tfrecords"
+
+    DATASET_DIR = os.path.join(data_dir, "image")
+    TFRECORD_DIR = os.path.join(data_dir, "tfrecord")
+
+    train_record = os.path.join(TFRECORD_DIR, "train.tfrecords")
+    test_record = os.path.join(TFRECORD_DIR, "test.tfrecords")
+    val_record = os.path.join(TFRECORD_DIR, "validation.tfrecords")
+
     CHECKPOINT_DIR = "./ckpt/"
 
     # placeholder
     x = tf.compat.v1.placeholder(tf.float32, [None, 224, 224])
     y = tf.compat.v1.placeholder(tf.float32, [None, num_digits])
 
-    lr = tf.Variable(0.001, dtype=tf.float32)
+    lr = tf.Variable(0.0003, dtype=tf.float32)
 
-    def read_and_decode(filename):
-        filename_queue = tf.compat.v1.train.string_input_producer([filename])
-        reader = tf.compat.v1.TFRecordReader()
-        _, serialized_example = reader.read(filename_queue)
-        features = tf.io.parse_single_example(
-            serialized_example,
-            features={
-                "image": tf.io.FixedLenFeature([], tf.string),
-                "labels": tf.io.FixedLenFeature([num_digits], tf.int64),
-            },
-        )
-        image = tf.io.decode_raw(features["image"], tf.uint8)
-        image = tf.reshape(image, [224, 224])
-        image = tf.cast(image, tf.float32) / 255.0
-        image = tf.subtract(image, 0.5)
-        image = tf.multiply(image, 2.0)
-        labels = tf.cast(features["labels"], tf.int32)
-        return image, labels
+    train_images, _, train_labels = read_and_decode(train_record, num_digits)
+    test_images, _, test_labels = read_and_decode(train_record, num_digits)
+    val_images, _, val_labels = read_and_decode(train_record, num_digits)
 
-    image, labels = read_and_decode(TFRECORD_FILE)
+    (train_image_batch, train_label_batch,) = tf.compat.v1.train.shuffle_batch(
+        [train_images, train_labels],
+        batch_size=batch_size,
+        capacity=1075,
+        min_after_dequeue=1000,
+        num_threads=128,
+    )
 
-    (image_batch, label_batch,) = tf.compat.v1.train.shuffle_batch(
-        [image, labels],
+    (test_image_batch, test_label_batch,) = tf.compat.v1.train.shuffle_batch(
+        [test_images, test_labels],
+        batch_size=batch_size,
+        capacity=1075,
+        min_after_dequeue=1000,
+        num_threads=128,
+    )
+
+    (val_image_batch, val_label_batch,) = tf.compat.v1.train.shuffle_batch(
+        [val_images, val_labels],
         batch_size=batch_size,
         capacity=1075,
         min_after_dequeue=1000,
@@ -196,15 +232,9 @@ def train(num_digits, batch_size):
         is_training=True,
     )
 
-    # gpu_options = tf.GPUOptions(allow_growth=True)
-
-    # with tf.Session(config=tf.ConfigProto(log_device_placement=False,allow_soft_placement=True,gpu_options=gpu_options)) as sess:
-    #     gpu_options = tf.GPUOptions(allow_growth=True)
-    #     tf.Session(config=tf.ConfigProto(log_device_placement=False,allow_soft_placement=True,gpu_options=gpu_options))
-
     with tf.compat.v1.Session() as sess:
 
-        X = tf.reshape(x, [BATCH_SIZE, 224, 224, 1])
+        X = tf.reshape(x, [batch_size, 224, 224, 1])
 
         logits, end_pintos = network.construct(X)
 
@@ -226,43 +256,48 @@ def train(num_digits, batch_size):
 
         saver = tf.compat.v1.train.Saver()
         sess.run(tf.compat.v1.global_variables_initializer())
-        # saver.restore(sess, './ckpt/crack_captcha-10000.ckpt')
-        # sess.run(tf.local_variables_initializer())
+
         coord = tf.train.Coordinator()
         threads = tf.compat.v1.train.start_queue_runners(sess=sess, coord=coord)
 
-        for i in range(10001):
-            b_image, b_labels = sess.run([image_batch, label_batch])
-            sess.run(
-                optimizer,
+        for i in range(100000):
+            b_train_images, b_train_labels = sess.run([train_image_batch, train_label_batch])
+            _, train_loss = sess.run(
+                [optimizer, total_loss],
                 feed_dict={
-                    x: b_image,
-                    y: b_labels,
+                    x: b_train_images,
+                    y: b_train_labels,
                 },
             )
 
-            if i % 100 == 0:
-                if i % 5000 == 0:
+            if i % STAT_PRINT_ITERS == 0:
+                if i % LEARNING_RATE_DROP_ITERS == 0 and i > 0:
                     sess.run(tf.compat.v1.assign(lr, lr / 3))
-                acc, loss_ = sess.run(
-                    [accuracy, total_loss],
+                b_test_images, b_test_labels = sess.run([test_image_batch, test_label_batch])
+                test_acc, = sess.run(
+                    [accuracy],
                     feed_dict={
-                        x: b_image,
-                        y: b_labels,
+                        x: b_test_images,
+                        y: b_test_labels,
+                    },
+                )
+
+                b_val_images, b_val_labels = sess.run([val_image_batch, val_label_batch])
+                val_acc, = sess.run(
+                    [accuracy],
+                    feed_dict={
+                        x: b_val_images,
+                        y: b_val_labels,
                     },
                 )
                 learning_rate = sess.run(lr)
                 print(
-                    "Iter: %d, Loss:%.3f, Accuracy: %.3f,  Learning_rate:%.7f"
-                    % (i, loss_, acc, learning_rate)
+                    f"Iter: {i}, Train loss: {train_loss:.3}, Test acc: {test_acc:.3}, Val acc: {val_acc:.3}, LR: {learning_rate:.7}"
                 )
 
-                # if acc0 > 0.9 and acc1 > 0.9 and acc2 > 0.9 and acc3 > 0.9 :
-
-                if i % 5000 == 0:
-                    # saver.save(sess,'./ckpt/crack_captcha.ckpt', global_step=1)
+                if i % CHECKPOINT_ITERS == 0:
                     saver.save(
-                        sess, CHECKPOINT_DIR + "crack_captcha-" + str(i) + ".ckpt"
+                        sess, os.path.join(CHECKPOINT_DIR, f"crack_captcha_{i}.ckpt")
                     )
                     print("Save model %s------" % str(i))
                     continue
@@ -270,37 +305,51 @@ def train(num_digits, batch_size):
         coord.join(threads)
 
 
+def find_oldest_file_in_directory(directory, pattern):
+    if not os.path.exists(directory) or not os.path.isdir(directory):
+        return None
+
+    # Initialize variables to track the oldest file and its timestamp
+    oldest_file = None
+    oldest_timestamp = float('inf')  # Start with a high value
+
+    # Compile the regular expression pattern
+    regex = re.compile(pattern)
+
+    # Traverse only the files in the directory (excluding subdirectories)
+    for filename in os.listdir(directory):
+        if regex.match(filename):
+            file_path = os.path.join(directory, filename)
+            file_timestamp = os.path.getmtime(file_path)
+
+            # Update the oldest file if needed
+            if file_timestamp < oldest_timestamp:
+                oldest_file = file_path
+                oldest_timestamp = file_timestamp
+
+    return oldest_file
+
+
+def get_most_recent_checkpoint(data_dir):
+    CHECKPOINT_DIR = os.path.join(data_dir, "ckpt")
+    path = find_oldest_file_in_directory(CHECKPOINT_DIR, CHECKPOINT_PREFIX + r"\d+\.ckpt\..+")
+    return ".".join(path.split(".")[:-1])
+
+
 # 5-------------
-def test(num_digits):
+def test(num_digits, data_dir):
     batch_size = 1
-    TFRECORD_FILE = "./image/tfrecord/test.tfrecords"
+    
+    DATASET_DIR = os.path.join(data_dir, "image")
+    TFRECORD_DIR = os.path.join(data_dir, "tfrecord")
+    CHECKPOINT_DIR = os.path.join(data_dir, "ckpt")
+
+    TFRECORD_FILE = os.path.join(TFRECORD_DIR, "test.tfrecords")
 
     x = tf.compat.v1.placeholder(tf.float32, [None, 224, 224])
 
-    def read_and_decode(filename):
-        filename_queue = tf.compat.v1.train.string_input_producer([filename])
-        reader = tf.compat.v1.TFRecordReader()
-        _, serialized_example = reader.read(filename_queue)
-        features = tf.io.parse_single_example(
-            serialized_example,
-            features={
-                "image": tf.io.FixedLenFeature([], tf.string),
-                "labels": tf.io.FixedLenFeature([num_digits], tf.int64),
-            },
-        )
-        image = tf.io.decode_raw(features["image"], tf.uint8)
-        image_raw = tf.reshape(image, [224, 224])  # raw data
-
-        image = tf.reshape(image, [224, 224])
-        image = tf.cast(image, tf.float32) / 255.0  # standardlize
-        image = tf.subtract(image, 0.5)
-        image = tf.multiply(image, 2.0)
-
-        labels = tf.cast(features["labels"], tf.int32)
-        return image, image_raw, labels
-
     # get label
-    image, image_raw, labels = read_and_decode(TFRECORD_FILE)
+    image, image_raw, labels = read_and_decode(TFRECORD_FILE, num_digits)
     # print(len(sess.run(image)))
     (image_batch, image_raw_batch, label_batch,) = tf.compat.v1.train.shuffle_batch(
         [image, image_raw, labels],
@@ -323,7 +372,7 @@ def test(num_digits):
 
         sess.run(tf.compat.v1.global_variables_initializer())
         saver = tf.compat.v1.train.Saver()
-        saver.restore(sess, "./ckpt/crack_captcha-10000.ckpt")
+        saver.restore(sess, get_most_recent_checkpoint(data_dir))
 
         coord = tf.train.Coordinator()
         threads = tf.compat.v1.train.start_queue_runners(sess=sess, coord=coord)
@@ -362,9 +411,10 @@ if __name__ == "__main__":
     NUM_SAMPLES = 1000
     NUM_TEST = 50
     BATCH_SIZE = 16
+    DATA_DIR = "."
 
-    generate_folder()
-    generate_train_data(num_samples=NUM_SAMPLES, num_digits=NUM_DIGITS)
-    save_as_tf(num_test=NUM_TEST)
-    train(num_digits=NUM_DIGITS, batch_size=BATCH_SIZE)
-    test(num_digits=NUM_DIGITS)
+    generate_folder(data_dir=DATA_DIR)
+    generate_train_data(num_samples=NUM_SAMPLES, num_digits=NUM_DIGITS, data_dir=DATA_DIR)
+    save_as_tf(num_test=NUM_TEST, data_dir=DATA_DIR)
+    train(num_digits=NUM_DIGITS, batch_size=BATCH_SIZE, data_dir=DATA_DIR)
+    test(num_digits=NUM_DIGITS, data_dir=DATA_DIR)
